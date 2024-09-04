b985d286759918f4e1db600e81218f0e6647e8db
Filipe Colla <173160383+collafilipe@users.noreply.github.com>
2024-09-04 15:46:29 -0300

    Delete AT1 directory

--- AT1/modelos/telefone.ts
+++
@@ -1,9 +0,0 @@
-export default class Telefone {
-    public ddd: string;
-    public numero: string;
-
-    constructor(ddd: string, numero: string) {
-        this.ddd = ddd;
-        this.numero = numero;
-    }
-}
