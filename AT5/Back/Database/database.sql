54dcea16be4b0a81f2b541079961b36fc5052d15
Filipe Colla <filipe10colla@gmail.com>
2024-10-07 00:01:50 -0300

    Ajustes Finais

---
+++ AT5/Back/Database/database.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE atv5;
---
+++ AT5/Back/Database/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO quarto (tipo, quartoStatus) VALUES 
+('Casal Simples', 'Disponivel'),
+("Familia Simples", "Disponivel"),
+("Familia Mais", "Disponivel"),
+("Familia Super", "Disponivel"),
+("Solteiro Simples", "Disponivel"),
+("Solteiro Mais", "Disponivel");
